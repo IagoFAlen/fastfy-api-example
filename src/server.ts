6ab16d336bfa57f8f58a6793d86a9df6a6b01b4f
Iago Figueira <iago.f.alen@gmail.com>
2024-10-14 19:31:37 -0300

    Finished test

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,6 @@
-import fastify from "fastify";
-import crypto from 'node:crypto'
-import { knex } from "./database";
-import { env } from "./env";
-import { transactionsRoutes } from "./routes/transactions";
-import cookie from "@fastify/cookie";
+import { app } from "./app"
+import { env } from "./env"
 
-const app = fastify()
-
-app.register(cookie)
-app.register(transactionsRoutes, {
-	prefix: 'transactions',
-})
   
 app.listen({
 	port: env.PORT,
